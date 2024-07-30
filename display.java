7f49a62a564509c32111e488be8bc674a8c923d5
Shuvojit Samanta <142198651+shuvojitss@users.noreply.github.com>
2024-07-30 16:51:02 +0530

    Create display.java

---
+++ display.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class display{
+	public static void main(String args[])
+	{
+	Scanner sc = new Scanner(System.in);
+	int n,i;
+	String s;
+	System.out.print("Enter the value of n: ");
+	n=sc.nextInt();
+	System.out.print("Enter the string :");
+	s=sc.next();
+	for(i=0;i<n;i++){
+	System.out.println(s);
+	}
+}
+}
+	
+	

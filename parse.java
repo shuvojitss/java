c54ceddbfcb73cdc434975bbbd04182ae5dd59bb
Shuvojit Samanta <142198651+shuvojitss@users.noreply.github.com>
2024-07-30 16:50:33 +0530

    Create parse.java

---
+++ parse.java
@@ -0,0 +1,10 @@
+class parse{
+	public static void main(String args[]){
+	int i=Integer.parseInt(args[0]);
+	System.out.println(i);
+	int j=Integer.parseInt(args[i]);
+	System.out.println(j);
+	int c=i+j;
+	System.out.println(c);
+	}
+}
